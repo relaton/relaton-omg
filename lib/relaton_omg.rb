343ca5938728f4daed84fd064d104a461adc40b2
Andrei Kislichenko <android.2net@gmail.com>
2023-09-19 17:59:33 -0400

    use gem version as trigger to flush cache relaton/relaton#115

--- lib/relaton_omg.rb
+++ lib/relaton_omg.rb
@@ -11,9 +11,9 @@ module RelatonOmg
   # Returns hash of XML reammar
   # @return [String]
   def self.grammar_hash
-    gem_path = File.expand_path "..", __dir__
-    grammars_path = File.join gem_path, "grammars", "*"
-    grammars = Dir[grammars_path].sort.map { |gp| File.read gp }.join
-    Digest::MD5.hexdigest grammars
+    # gem_path = File.expand_path "..", __dir__
+    # grammars_path = File.join gem_path, "grammars", "*"
+    # grammars = Dir[grammars_path].sort.map { |gp| File.read gp }.join
+    Digest::MD5.hexdigest RelatonOmg::VERSION + RelatonBib::VERSION # grammars
   end
 end
